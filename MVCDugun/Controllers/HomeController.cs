dbf7a2786da4c471a02ac7cbb0128ad470115504
YunusDinc <yunusdinc1988@hotmail.com>
2016-11-21 11:08:19 +0200

    Sutup

---
+++ MVCDugun/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDugun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Slider() //<div id=home>
+        {
+            return View();
+        }
+
+        public ActionResult Story()
+        {
+            return View();
+        }
+
+        public ActionResult Timer()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Wedding()
+        {
+            return View();
+        }
+
+        public ActionResult Rezervasyon()
+        {
+            return View();
+        }
+    }
+}
